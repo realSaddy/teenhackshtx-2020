1c9de1824d867f60d7af31b50d15e2d0c046aa6c
realSaddy <jrp30074@gmail.com>
2020-05-29 22:48:11 -0500

    add header to index

--- client/src/pages/Index/index.js
+++ client/src/pages/Index/index.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Header from "./Header";
 
 class Index extends React.Component {
-
-
-formRow() {
+  formRow() {
     const paperStyle = {
-        padding: "10px",
-        textAlign: "center",
-        backgroundColor: "grey",
-        alignItems: "stretch"
-
-    }
+      padding: "10px",
+      textAlign: "center",
+      backgroundColor: "grey",
+      alignItems: "stretch",
+    };
     return (
       <React.Fragment>
         <Grid justify="space-evenly" item md={4}>
@@ -27,31 +24,29 @@ formRow() {
         </Grid>
       </React.Fragment>
     );
-}
+  }
 
-render() {
+  render() {
     const gridStyle = {
-        alignItems: "stretch"
-    }
-    return(
-        <React.Fragment>
-
-            <Grid justify="space-evenly" container spacing={1}>
-                <Grid justify="space-evenly" container item md={12} spacing={1} >
-                {this.formRow()}
-                </Grid>
-                <Grid justify="space-evenly" container item md={12} spacing={1} >
-                {this.formRow()}
-                </Grid>
-                <Grid justify="space-evenly" container item md={12} spacing={1} >
-                {this.formRow()}
-                </Grid>
-            </Grid>
-        </React.Fragment>
-        
-        
+      alignItems: "stretch",
+    };
+    return (
+      <React.Fragment>
+        <Header />
+        <Grid justify="space-evenly" container spacing={1}>
+          <Grid justify="space-evenly" container item md={12} spacing={1}>
+            {this.formRow()}
+          </Grid>
+          <Grid justify="space-evenly" container item md={12} spacing={1}>
+            {this.formRow()}
+          </Grid>
+          <Grid justify="space-evenly" container item md={12} spacing={1}>
+            {this.formRow()}
+          </Grid>
+        </Grid>
+      </React.Fragment>
     );
-}
+  }
 }
 
 export default Index;
