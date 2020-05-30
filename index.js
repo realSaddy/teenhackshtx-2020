8813ffee8793d3ecc4852e1f2ef2339312c6aba5
realSaddy <jrp30074@gmail.com>
2020-05-30 16:23:55 -0500

    Hosting deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -24,6 +25,16 @@ app.get("/api/user/:id", (req, res) => getUser(req, res));
 app.post("/api/search", (req, res) => search(req, res));
 app.post("/api/claim", (req, res) => claim(req, res));
 
-app.listen(8080, () => {
-  console.log("\x1b[32m✓\x1b[0m Running on port 8080!");
-});
+if (process.env.DEPLOY === "true") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+  app.listen(80, () => {
+    console.log("\x1b[32m✓\x1b[0m Running on port 80!");
+  });
+} else {
+  app.listen(8080, () => {
+    console.log("\x1b[32m✓\x1b[0m Running on port 8080!");
+  });
+}
