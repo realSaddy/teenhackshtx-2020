2fcebb022963ffccfcc5299f28d1b5a610aa3933
realSaddy <jrp30074@gmail.com>
2020-05-29 19:20:27 -0500

    routes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/api/test", (req, res) => {
+  res.send("Test");
+});
+
+app.listen(8080, () => {
+  console.log("\x1b[32m✓\x1b[0m Running on port 8080!");
+});
