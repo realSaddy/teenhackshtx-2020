35650cb1779bea82ca0c64784785941c239baae7
realSaddy <jrp30074@gmail.com>
2020-05-29 19:18:40 -0500

    routes

---
+++ client/src/pages/Index/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="App">Hello!</div>;
+  }
+}
+
+export default Index;
